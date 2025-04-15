6442b5bd1c6679f9e00b251f1eebc967cbe298c8
Daniel Landa <landadlh603@gmail.com>
2025-04-16 01:57:46 +0200

    Añadir modelos y vistas para Roles, Companies, Comments, Payments y Offers en la API

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
-  
+
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Roles, Companies, Comments, Payments, Offers
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,10 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-
+    admin.add_view(ModelView(Roles, db.session))
+    admin.add_view(ModelView(Companies, db.session))
+    admin.add_view(ModelView(Comments, db.session))
+    admin.add_view(ModelView(Payments, db.session))
+    admin.add_view(ModelView(Offers, db.session))
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean,ForeignKey,Float,Integer, DateTime
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -17,12 +18,111 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+    #role_id: Mapped[int] = mapped_column(ForeignKey('role_id'),nullable=True)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    #relations
+    # comments = relationship('Comments',back_populates='users')
+    # payments = relationship('Payments',back_populates='users')
+    
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+    
+
+class Roles(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120),unique=True,nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+    
+    
+
+
+class Companies(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120))
+    bussiness_email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)#email corporativo
+    logo_url: Mapped[int]
+    company_url:Mapped[str] = mapped_column(String(250),nullable = False)
+    #role_id: Mapped[int] = mapped_column(ForeignKey('role_id'))
+
+    #relations
+
+    #serialize
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "bussiness_email": self.bussiness_email,
+            "logo_url": self.logo_url,
+            "company_url": self.company_url,
+        }
+
+class Offers(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(120), nullable=False) 
+    description: Mapped[str] = mapped_column(String(520))
+    price:Mapped[int] = mapped_column(Integer, nullable=False)
+    #type:Mapped[str] = mapped_column(String(120))
+    image_url: Mapped[int]
+    #company_id: Mapped[int] = mapped_column(ForeignKey('company_id'))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
+    #relations
+
+    #serialize
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "price": self.price,
+            "image_url": self.image_url,
+            "created_at": self.created_at,
+        }
+class Comments(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    # user_id: Mapped[int] = mapped_column(ForeignKey('user_id'))
+    # offer_id: Mapped[int] = mapped_column(ForeignKey('offer_id'))
+    content: Mapped[str] = mapped_column(String(520))
+    created_at: Mapped[str] = mapped_column(String(120))
+
+
+    #relations
+
+    #serialize
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "created_at": self.created_at,
+        }
+class Payments(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    # user_id: Mapped[int] = mapped_column(ForeignKey('user_id'))
+    # offer_id: Mapped[int] = mapped_column(ForeignKey('offer_id'))
+    amount: Mapped[int] = mapped_column()
+    payment_method: Mapped[str] = mapped_column(String(120))
+    created_at: Mapped[str] = mapped_column(String(120))
+    status:Mapped[str] = mapped_column(String(120))
+
+    #relations
+
+    #serialize
+    def serialize(self):
+        return {
+            "id": self.id,
+            "amount": self.amount,
+            "payment_method": self.payment_method,
+            "created_at": self.created_at,
+            "status": self.status,
+        }
+
