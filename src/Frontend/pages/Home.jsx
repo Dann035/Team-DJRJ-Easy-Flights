d8e179a9c0305926dee204d33305d94691b232c3
Daniel Landa <landadlh603@gmail.com>
2025-04-23 19:32:55 +0200

    primeros pasos de home

--- src/Frontend/pages/Home.jsx
+++ src/Frontend/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import Login from "./Login/Login.jsx";
 import Tagline from "../components/Tagline/Tagline.jsx";
-import Offerts from "./Offerts/Offerts.jsx";
 import TravelShow from "../components/TravelShowCase/TravelShow.jsx";
 import Comments from "../components/Comments/Comments.jsx";
+import Features from "../components/Features/Features.jsx";
+
 
 export const Home = () => {
 
@@ -12,7 +13,7 @@ export const Home = () => {
 			<Tagline />
             <TravelShow />
             <Comments />
-			<Offerts />
+            <Features />
 		</>
     );
 };
