6d2c5e4a9f3b24740dbd9ae630dd5323524220b9
Daniel Landa <landadlh603@gmail.com>
2025-04-24 04:40:47 +0200

    Añadir estilos y animaciones al componente Features

--- src/Frontend/components/Features/Features.jsx
+++ src/Frontend/components/Features/Features.jsx
@@ -1,33 +1,33 @@
-import React from "react";
+import "./Features.css";
 
 function Features() {
     return (
-        <section>
+        <section className="container text-center">
             <div>
                 <p>Explore</p>
-                <h1>Discover Your Next Adventure with Us</h1>
+                <h1 className="fw-bold">Discover Your Next Adventure with Us</h1>
                 <p>Uncover breathtaking destinations that inspire wanderlust. Our curated travel packages offer unique experiences tailored just for you.</p>
             </div>
-            <div>
-                <section>
+            <div className="d-flex justify-content-between p-3 gap-3">
+                <section className="box-feature">
                     <i>Icon</i>
                     <h3>Top Travel Destinations You Can't Miss</h3>
                     <p>From vibrant cities to serene landscapes, explore it all.</p>
                 </section>
-                <section>
+                <section className="box-feature">
                     <i>Icon</i>
                     <h3>Tailored Travel Packages for Every Traveler</h3>
                     <p>Experience the world with packages designed for you.</p>
                 </section>
-                <section>
+                <section className="box-feature">
                     <i>Icon</i>
                     <h3>Unique Experiences Await at Every Turn</h3>
                     <p>Create unforgettable memories with our exclusive offerings.</p>
                 </section>
             </div>
-            <div>
-                <button>Learn More</button>
-                <button>Sign Up</button>
+            <div className="d-flex justify-content-center gap-3 mt-4 mb-3">
+                <button className="btn-feature">Learn More</button>
+                <button className="btn-feature">Sign Up</button>
             </div>
         </section>
     )
