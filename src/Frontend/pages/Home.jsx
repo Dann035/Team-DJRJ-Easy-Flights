c43fb4215545ef15bb90405206ad08c5757dbc4f
Daniel Landa <landadlh603@gmail.com>
2025-04-24 04:41:32 +0200

    Añadir animaciones y variables de color al CSS, e incluir el componente Features en la página de inicio

--- src/Frontend/pages/Home.jsx
+++ src/Frontend/pages/Home.jsx
@@ -3,7 +3,7 @@ import Login from "./Login/Login.jsx";
 import Tagline from "../components/Tagline/Tagline.jsx";
 import TravelShow from "../components/TravelShowCase/TravelShow.jsx";
 import Comments from "../components/Comments/Comments.jsx";
-
+import Features from "../components/Features/Features.jsx";
 
 export const Home = () => {
 
@@ -12,6 +12,7 @@ export const Home = () => {
 			<Tagline />
             <TravelShow />
             <Comments />
+            <Features />
 		</div>
     );
 };
