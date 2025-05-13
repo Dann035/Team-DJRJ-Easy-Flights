182d528646faa953a374d248b973601f689a3f9e
Daniel Landa <landadlh603@gmail.com>
2025-05-13 13:00:30 +0200

    Eliminar rutas de perfil de usuario y código de simulación de recuperación de contraseña

--- src/Backend/routes/__init__.py
+++ src/Backend/routes/__init__.py
@@ -7,7 +7,6 @@ from .offers import offers_bp
 from .company import company_bp
 from .comment import comments_bp
 from .payment import payment_bp
-from .user_profile_routes import profile_bp
 from .auth import auth_bp
 from .password_reset import pass_bp
 
@@ -24,6 +23,5 @@ api.register_blueprint(offers_bp)
 api.register_blueprint(company_bp)
 api.register_blueprint(payment_bp)
 api.register_blueprint(comments_bp)
-api.register_blueprint(profile_bp)
 api.register_blueprint(auth_bp)
 api.register_blueprint(pass_bp)
