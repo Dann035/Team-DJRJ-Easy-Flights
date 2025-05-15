3c759a1e2abeaa6469f834e096a48e83547c4d55
Daniel Landa <landadlh603@gmail.com>
2025-05-16 01:52:23 +0200

    Actualizar la ruta del directorio de archivos estáticos en app.py

--- src/app.py
+++ src/app.py
@@ -18,7 +18,7 @@ from flask_mail import Mail
 # from models import Person
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), '/public/')
+    os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
